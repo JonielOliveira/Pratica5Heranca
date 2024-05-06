88024fa6e5601b4f49b254c036879b23176071bd
Joniel R. de Oliveira <joniel.oliveira@fatec.sp.gov.br>
2024-05-06 10:51:22 -0300

    Create class Pessoa
    Create class Pessoa.

---
+++ Pratica5Heranca/src/pratica5heranca/Pessoa.java
@@ -0,0 +1,14 @@
+
+package pratica5heranca;
+
+public class Pessoa {
+
+    protected String RG;
+    protected String nome;
+    
+    public Pessoa(String RG, String nome){
+        this.RG = RG;
+        this.nome = nome;
+    }
+
+}
